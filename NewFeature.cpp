2a7621b9cacfb0769fd20fe31eaa4a0b7517fddd
MiguelChagas <108590753+MiguelChagas@users.noreply.github.com>
2024-04-09 14:01:45 -0300

    Criação da classe C com métodos MC1() MC2() e MC3() e D com métodos MD1() e MD2()

--- NewFeature.cpp
+++ NewFeature.cpp
@@ -27,22 +27,74 @@ private:
 
 public:
     B(int b1, float b2) : B1(b1), B2(b2) {}
+
     int getB1() const { return B1; }
     void setB1(int b1) { B1 = b1; }
 
     float getB2() const { return B2; }
     void setB2(float b2) { B2 = b2; }
 
-    void MB1() { std::cout << "Método MB1() chamado" << std::endl; }
-    void MB2() { std::cout << "Método MB2() chamado" << std::endl; }
+    void MB1() { std::cout << "Metodo MB1() chamado" << std::endl; }
+    void MB2() { std::cout << "Metodo MB2() chamado" << std::endl; }
+
+    void MB3() { std::cout << "Metodo MB3() chamado" << std::endl; }
 };
 
+class C {
+private:
+    std::string C1;
+    int C2;
+
+public:
+    C(std::string c1, int c2) : C1(c1), C2(c2) {}
+
+    std::string getC1() const { return C1; }
+    void setC1(std::string c1) { C1 = c1; }
+
+    int getC2() const { return C2; }
+    void setC2(int c2) { C2 = c2; }
+
+    void MC1() { std::cout << "Metodo MC1() chamado" << std::endl; }
+    void MC2() { std::cout << "Metodo MC2() chamado" << std::endl; }
+
+    void MC3() { std::cout << "Metodo MC3() chamado" << std::endl; }
+};
+
+class D {
+private:
+    int D1;
+    float D2;
+
+public:
+    D(int d1, float d2) : D1(d1), D2(d2) {}
+
+    int getD1() const { return D1; }
+    void setD1(int d1) { D1 = d1; }
+
+    float getD2() const { return D2; }
+    void setD2(float d2) { D2 = d2; }
+
+    void MD1() { std::cout << "Metodo MD1() chamado" << std::endl; }
+    void MD2() { std::cout << "Metodo MD2() chamado" << std::endl; }
+};
 int main() {
     A objA(10, 3.14);
     B objB(20, 6.28);
 
     objA.MA1();
     objB.MB2();
+      
+    C objetoC("Exemplo", 42);
+
+    objetoC.MC1();
+    objetoC.MC2();
+    objetoC.MC3();
+
+    D objetoD(10, 3.14); 
+
+    objetoD.MD1();
+    objetoD.MD2(); 
+
 
     return 0;
 }
